971b4012a2727ba8a447bd40729664ffa8b7d4dd
Pablo Valor <pablovalor89@gmail.com>
2015-08-15 18:11:55 -0300

    Created EF model from DB Dummy from Sql Server Express

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Data;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
-        //
         // GET: /Home/
+        BlogDBEntities db = new BlogDBEntities();
 
         public ActionResult Index()
         {
-            return View();
+            var _model = db.Posts.ToList();
+
+            return View(_model);
         }
 
     }
