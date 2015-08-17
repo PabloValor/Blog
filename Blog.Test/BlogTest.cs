7420430f36bfdd5c6aec777850016adee3f55ecc
Pablo Valor <pablovalor89@gmail.com>
2015-08-17 18:35:36 -0300

    Added Nunit unit test project

---
+++ Blog.Test/BlogTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Blog.Controllers;
+
+namespace Blog.Test
+{
+    [TestFixture]
+    public class BlogTest
+    {
+        /// <summary>
+        /// Test for action methods from HomeController
+        /// </summary>
+        [Test]
+        public void TestHomeController()
+        {
+            int number = 2;
+
+            NUnit.Framework.Assert.AreEqual(number, 2);            
+        }
+    }
+}
